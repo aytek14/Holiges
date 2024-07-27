44a097db7f21b3fdda8476a5aa8805e4c013c94b
Aytek Gucetil <aytekgucetil@gmail.com>
2024-07-27 16:01:22 +0300

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from "next/app";
+import React, {useEffect} from 'react';
 
 const App = ({ Component, pageProps }: AppProps) => {
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && 'serviceWorker' in navigator && process.env.NODE_ENV !== 'development') {
+      navigator.serviceWorker.register('/sw.js').then(registration => {
+        console.log('PWA service worker ready');
+        registration.update();
+      }).catch(error => {
+        console.log('PWA service worker registration failed', error);
+      });
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
